1d68c85886504c3812594914b024cab333456176
sequomics <sequomics@gmail.com>
2015-12-29 16:26:50 +0530

    Initial commit
    metadata of AFPkb®.

--- afpkb/__init__.py
+++ afpkb/__init__.py
@@ -1,3 +1,5 @@
 """Antifreeze Proteins (AFPs) KnowledgeBase by Sequomics Corporation."""
 __author__ 	= 'Prabhat Kumar'
-__license__ 	= 'Apache License'
+__license__ = 'Apache License'
+__version__ = '1.0.0'
+__release__ = __version__ # + '-dev' # comment out '-dev' before a release
