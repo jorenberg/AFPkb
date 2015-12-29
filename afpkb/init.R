4a53eccd85f7e9bc0fb456d5b56ca509ef3e76ff
Sequømics <sequomics@users.noreply.github.com>
2015-12-30 00:06:40 +0530

    Initial commit
    metadata of AFPkb®.

--- afpkb/init.R
+++ afpkb/init.R
@@ -1,1 +1,11 @@
 #!/usr/bin/env r
+# ============================================================================
+# AFPkb® [v1.0.0]
+# Antifreeze Proteins (AFPs) KnowledgeBase.
+# Date: 30-12-2015
+# Company: Sequømics Corporation, http://sequomics.com/
+# LICENSE: Apache, Version 2.0
+# Architecture and Code Handcrafted by Prabhat Kumar, http://prabhatkumar.org/
+# Contributor(s): Aiswarya Thomas.
+# Copyright © 2014 - 2015, Sequømics Corporation, All rights reserved.
+# ============================================================================
