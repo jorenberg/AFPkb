75f7ecc68d9e3a543c6a8968ed3d8b4de84ce879
Prabhat Kumar <prabhat.genome@gmail.com>
2016-02-29 22:26:13 +0530

    Initial commit
    AFPkb® - Isoelectric Points of Amino Acid(s).

--- afpkb/app/settings/config.py
+++ afpkb/app/settings/config.py
@@ -29,3 +29,30 @@ molecular_weights = {
     'B':132.6108,   # Apartic acid or asparagine
     'Z':146.6375    # Glutamic acid or glutamine
 }
+
+# Isoelectric Points of Amino Acid(s).
+# ------------------------------------
+isoelectric_points = {
+    'A':06.02,      # Alanine
+    'C':05.14,      # Cysteine
+    'D':02.87,      # Aspartic acid
+    'E':03.22,      # Glutamic acid
+    'F':05.48,      # Phenylalanine
+    'G':05.97,      # Glycine
+    'H':07.58,      # Histidine
+    'I':06.02,      # Isoleucine
+    'K':09.74,      # Lysine
+    'L':05.98,      # Leucine
+    'M':05.75,      # Methionine
+    'N':05.41,      # Asparagine
+    'P':06.10,      # Proline
+    'Q':05.65,      # Glutamine
+    'R':10.76,      # Arginine
+    'S':05.68,      # Serine
+    'T':06.53,      # Threonine
+    'V':05.97,      # Valine
+    'W':05.88,      # Tryptophan
+    'Y':05.65,      # Tyrosine
+    'B':04.14,      # Apartic acid or asparagine
+    'Z':04.435      # Glutamic acid or glutamine
+}
