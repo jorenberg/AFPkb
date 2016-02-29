ff04f695e1aef2496776abd8bb001ed45f9251df
Prabhat Kumar <prabhat.genome@gmail.com>
2016-02-29 22:05:41 +0530

    Initial commit
    AFPkb® - Molecular Weights of Amino Acid(s).

--- afpkb/app/settings/config.py
+++ afpkb/app/settings/config.py
@@ -2,3 +2,30 @@
 # -*- coding: utf-8 -*-
 
 __author__  = 'Prabhat Kumar, prabhat.genome@gmail.com'
+
+# Molecular Weights of Amino Acid(s).
+# -----------------------------------
+molecular_weights = {
+    'A':089.0935,   # Alanine
+    'C':121.1590,   # Cysteine
+    'D':133.1032,   # Aspartic acid
+    'E':147.1299,   # Glutamic acid
+    'F':165.1900,   # Phenylalanine
+    'G':075.0669,   # Glycine
+    'H':155.1552,   # Histidine
+    'I':131.1736,   # Isoleucine
+    'K':146.1882,   # Lysine
+    'L':131.1736,   # Leucine
+    'M':149.2124,   # Methionine
+    'N':132.1184,   # Asparagine
+    'P':115.1310,   # Proline
+    'Q':146.1451,   # Glutamine
+    'R':174.2017,   # Arginine
+    'S':105.0930,   # Serine
+    'T':119.1197,   # Threonine
+    'V':117.1469,   # Valine
+    'W':204.2262,   # Tryptophan
+    'Y':181.1894,   # Tyrosine
+    'B':132.6108,   # Apartic acid or asparagine
+    'Z':146.6375    # Glutamic acid or glutamine
+}
